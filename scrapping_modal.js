041c14c42353d0ae8d69aa0c72ac428b5cb59971
Dman89 <upnvapesd@gmail.com>
2016-11-07 15:55:05 -0800

    Added Modal For Reference

---
+++ scrapping_modal.js
@@ -0,0 +1,13 @@
+barModal = {
+  name: String,
+  phone: Number,
+  address: String,
+  address2: String,
+  city: String,
+  zipCode: Number,
+  state: String,
+  beers: {
+    brewery: String,
+    beer: String
+  }
+}
